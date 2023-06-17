87c86b3a20522ee6e28a5a5b30062c4cc261b139
Acaibrid <95097635+MagicXcode@users.noreply.github.com>
2023-06-17 02:45:23 -0400

    fas
    dfas

--- vite/headpage/src/components/1.vue
+++ vite/headpage/src/components/1.vue
@@ -1,82 +1,235 @@
 
 <template>
-    <div class="timeline" ref="timeline">
-      <div class="item" v-for="(item, index) in items" :key="index" :class="{ active: index === activeIndex }">
-        <div class="date">{{ item.date }}</div>
-        <div class="content">{{ item.content }}</div>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  
-  export default {
-    setup() {
-      const timeline = ref(null);
-      const items = [
-        { date: '2022-01-01', content: '事件1' },
-        { date: '2022-01-02', content: '事件2' },
-        { date: '2022-01-03', content: '事件3' },
-        { date: '2022-01-04', content: '事件4' },
-        { date: '2022-01-05', content: '事件5' },
-        { date: '2022-01-06', content: '事件6' },
-        { date: '2022-01-07', content: '事件7' },
-      ];
-      let activeIndex = 0;
-  
-      onMounted(() => {
-        window.addEventListener('scroll', handleScroll);
-      });
-  
-      const handleScroll = () => {
-        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        const timelineRect = timeline.value.getBoundingClientRect();
-        const items = timeline.value.querySelectorAll('.item');
-        const itemRects = Array.from(items).map((item) => item.getBoundingClientRect());
-  
-        for (let i = 0; i < itemRects.length; i++) {
-          const itemRect = itemRects[i];
-          if (itemRect.top <= timelineRect.top && itemRect.bottom >= timelineRect.bottom) {
-            activeIndex = i;
-          }
-        }
-  
-        timeline.value.querySelector('.active').classList.remove('active');
-        items[activeIndex].classList.add('active');
-      };
-  
-      return {
-        timeline,
-        items,
-        activeIndex,
-        handleScroll,
-      };
-    },
-  };
-  </script>
-  
-  <style>
-  .timeline {
+  <div>
+    <div class="wai">
+            <div class="music-card">
+                <div class="header">
+                    <h2 class="title">巴哈姆特音乐集卡</h2>
+                    <h3 class="subtitle">BAHAMUTE</h3>
+                </div>
+                <div class="timeline" ref="timeline">
+                    <div class="item" data-text="《你的孤独，虽败犹荣》">
+                        <div class="content">
+                            <img src="../img/01.jpg" alt="" class="img">
+                            <h2 class="content-title">2002</h2>
+                            <p class="content-desc">
+                                如果你停止，就是谷底。如果你还在继续，
+                                就是上坡。这是我听过关于人生低谷最好的阐述。
+                            </p>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/02.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/03.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/04.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/05.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc " >
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/06.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/07.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+
+                        <div class="item" data-text="《你的孤独，虽败犹荣》">
+                            <div class="content">
+                                <img src="../img/08.jpg" alt="" class="img">
+                                <h2 class="content-title">2002</h2>
+                                <p class="content-desc">
+                                    如果你停止，就是谷底。如果你还在继续，
+                                    就是上坡。这是我听过关于人生低谷最好的阐述。
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue';
+
+</script>
+<style scoped lang="less">
+
+
+.header {
+    width: 100%;
+    text-align: center;
+    margin-bottom: 80px;
     position: relative;
-    height: 500px;
-    overflow-y: scroll;
-  }
-  
-  .item {
+}
+
+.title {
+    color: #fff;
+    font-size: 46px;
+    font-weight: normal;
+    margin: 0;
+}
+
+.content-title {
+    font-weight: normal;
+    font-size: 66px;
+    margin: -10px 0 0 0;
+    transition: 0.4s;
+    padding: 0 10px;
+    box-sizing: border-box;
+    color: #fff;
+}
+
+.content-desc {
+    margin: 0;
+    font-size: 15px;
+    box-sizing: border-box;
+    color: rgba(255, 255, 255, 0.7);
+    line-height: 25px;
+}
+
+.wai {
+    display: inline-block;
+    position: absolute;
+    height: auto;
+    width: calc(100% - 200px);
+    top: 40px;
+    left: 200px;
+
+    .music-card {
+        background: linear-gradient(45deg, #fbda61, #fd69d1);
+        transition: 0.3s ease 0s;
+        background-attachment: fixed;
+        background-size: cover;
+
+
+
+    }
+}
+
+.wai:before {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    content: "";
+}
+
+
+
+.timeline {
+    display: flex;
+    margin: 0 auto;
+    flex-wrap: wrap;
+    flex-direction: column;
+    max-width: 1300px;
     position: relative;
-    margin-bottom: 20px;
-    padding: 10px;
-    background-color: #f7f7f7;
-    border-radius: 5px;
-  }
-  
-  .date {
-    font-weight: bold;
-  }
-  
-  .active {
-    background-color: #fff;
-    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  }
-  </style>
+}
+
+
+.timeline:before {
+    position: absolute;
+    left: 50%;
+    bottom: 50px;
+    width: 5px;
+    height: 100%;
+    content: "";
+    background: white;
+    opacity: 0.6;
+
+}
+
+.item {
+    padding: 40px 0;
+    opacity: 0.3;
+    filter: blur(2px);
+    transition: 0.5s;
+    box-sizing: border-box;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    transform: translateY(-80px);
+}
+
+.item:nth-child(even) {
+    align-self: flex-end;
+}
+
+
+.item:nth-child(even):before {
+    right: auto;
+    text-align: right;
+    left: calc(-100% - 56px);
+    padding-left: 0;
+    border-left: none;
+    border-right: 2px solid rgba(255, 255, 255, 0.5);
+    padding-right: 15px;
+}
+
+
+.img {
+    max-width: 100%;
+    box-shadow: 0 10px 15px rgba(0, 0, 0, 0.4);
+}
+
+.subtitle {
+    color: rgba(255, 255, 255, 0.5);
+    font-size: 16px;
+    letter-spacing: 5px;
+    margin: 10px 0 0 0;
+    font-weight: normal;
+}
+
+</style>
